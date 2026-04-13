83c8124f8612b9053eaf67d668b18df6fed36db5
Vishal Kumar <vishalkumar33d@gmail.com>
2026-04-13 21:55:20 +0530

    Updated UI-1

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,29 @@
-import React from "react";
+import { NavLink } from "react-router-dom";
+
+import "./Navbar.css";
 
 export default function Navbar() {
   return (
     <nav className="navbar">
-      <h2>SVN EDUCONS</h2>
-      <ul>
-        <li>Home</li>
-        <li>Courses</li>
-        <li>Admission</li>
-        <li>Contact</li>
+
+      {/* LEFT: Logo + Brand */}
+      <NavLink to="/" className="brand">
+        <img src="/logo.png" alt="logo" className="brand-logo" />
+        <h2 className="brand-text">
+          SVN <span>EduCon</span>
+        </h2>
+      </NavLink>
+
+      {/* RIGHT: Navigation */}
+      <ul className="nav-links">
+        <li><NavLink to="/" className="link">Home</NavLink></li>
+        <li><NavLink to="/about" className="link">About</NavLink></li>
+        <li><NavLink to="/services" className="link">Services</NavLink></li>
+        <li><NavLink to="/courses" className="link">Courses</NavLink></li>
+        <li><NavLink to="/admission" className="link">Admission</NavLink></li>
+        <li><NavLink to="/contact" className="link">Contact</NavLink></li>
       </ul>
+
     </nav>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,13 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import { BrowserRouter as Router, Routes, Route, Link, BrowserRouter } from "react-router-dom";
+
+
 
 createRoot(document.getElementById('root')).render(
-  
-    <App />
+  <BrowserRouter>
+  <App />
+  </BrowserRouter>
+    
 
 )
